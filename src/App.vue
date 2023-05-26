f3a462c413afadf99acd9d146861f88adaf1c112
Taoshuaixing <1554307063@qq.com>
2023-05-26 17:28:29 +0800

    fix:重构项目架构

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <template>
-	<router-view></router-view>
+  <el-config-provider :locale="zhCn">
+    <router-view #="{ Component, route }">
+      <component :is="Component" :key="route.path" />
+    </router-view>
+  </el-config-provider>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { provide } from 'vue';
+  import zhCn from 'element-plus/lib/locale/lang/zh-cn';
+  import { initVisualData, injectKey, localKey } from '@/visual-editor/hooks/useVisualData';
 
-<style lang="less" scoped></style>
+  const visualData = initVisualData();
+  // 注入可视化编辑器所有配置
+  provide(injectKey, visualData);
+
+  const { jsonData } = visualData;
+
+  window.addEventListener('beforeunload', () => {
+    sessionStorage.setItem(localKey, JSON.stringify(jsonData));
+  });
+</script>
+
+<style lang="scss">
+  @import 'style/common';
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,28 +1,28 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 import NProgress from 'nprogress'; // progress bar
-import 'nprogress/nprogress.css'; // 进度条样式
+import 'nprogress/css/nprogress.css'; // 进度条样式
 
 NProgress.configure({ showSpinner: false }); // NProgress Configuration
 
 const routes: Array<RouteRecordRaw> = [
-	{
-		path: '/:pathMatch(.*)*',
-		component: () => import('@/layout/index.vue'),
-	},
+  {
+    path: '/:pathMatch(.*)*',
+    component: () => import('@/visual-editor/index.vue'),
+  },
 ];
 
 const router = createRouter({
-	history: createWebHashHistory(),
-	routes,
+  history: createWebHashHistory(),
+  routes,
 });
 
 router.beforeEach(() => {
-	NProgress.start(); // start progress bar
-	return true;
+  NProgress.start(); // start progress bar
+  return true;
 });
 
 router.afterEach(() => {
-	NProgress.done(); // finish progress bar
+  NProgress.done(); // finish progress bar
 });
 
 export default router;
