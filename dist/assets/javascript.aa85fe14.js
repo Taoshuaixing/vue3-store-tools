73d4a7817a132df6635d778aa0bb7d77486a3522
Taoshuaixing <1554307063@qq.com>
2023-06-20 16:16:05 +0800

    build:更新dist

--- dist/assets/javascript.aa85fe14.js
+++
@@ -1,7 +0,0 @@
-import{conf as e,language as t}from"./typescript.12db34d8.js";import"./index.1f93102d.js";import"./useVisualData.4da3e46b.js";import"./preload-helper.4bba4334.js";import"./index.72d2af62.js";import"./editorWorker.b1d4a0bc.js";import"./useAnimate.fc3dbea2.js";
-/*!-----------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Version: 0.33.0(4b1abad427e58dbedc1215d99a0902ffc885fcd4)
- * Released under the MIT license
- * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
- *-----------------------------------------------------------------------------*/var s=e,i={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:t.operators,symbols:t.symbols,escapes:t.escapes,digits:t.digits,octaldigits:t.octaldigits,binarydigits:t.binarydigits,hexdigits:t.hexdigits,regexpctl:t.regexpctl,regexpesc:t.regexpesc,tokenizer:t.tokenizer};export{s as conf,i as language};
